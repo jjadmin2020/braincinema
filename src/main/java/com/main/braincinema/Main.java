210d1db2a9b35161be49d5901d91509e4f8afd3e
Fedorenko Oleksandr <Oleksandr.Fedorenko@onseo.biz>
2020-01-16 13:42:19 +0200

    VJG-6 : Create brain-cinema project

---
+++ src/main/java/com/main/braincinema/Main.java
@@ -0,0 +1,10 @@
+package com.main.braincinema;
+
+/**
+ * Created by OFedorenko on 01/16/20 braincinema.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello BrainCinema");
+    }
+}
