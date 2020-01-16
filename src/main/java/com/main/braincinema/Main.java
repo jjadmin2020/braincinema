c2778884933260f6daf5fcea228743a26da78a51
YarVisDD <59877101+YarVisDD@users.noreply.github.com>
2020-01-16 19:43:28 +0200

    test commit Yar

--- src/main/java/com/main/braincinema/Main.java
+++ src/main/java/com/main/braincinema/Main.java
@@ -5,6 +5,6 @@ package com.main.braincinema;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello BrainCinema");
+        System.out.println("Hello World");
     }
 }
