bb4e24647482cb34b4a2ad5b144d117c04b569b4
Aleksandr <aleksandrburlutskiy@gmail.com>
2020-01-21 09:54:14 +0200

    Restoration of the original develop

--- src/main/java/com/main/braincinema/entity/User.java
+++ src/main/java/com/main/braincinema/entity/User.java
@@ -9,9 +9,4 @@ public class User {
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
-    private String city;
-    private int phoneNumber;
-    private String eMail;
-    private String password;
-
 }
