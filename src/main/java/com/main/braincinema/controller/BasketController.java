e229f048e4f48dff6f282d3e73267ee77f6aa931
jjadmin2020 <z.dikalchuk@gmail.com>
2020-01-22 16:31:06 +0200

    VJG-15 Update BasketController

--- src/main/java/com/main/braincinema/controller/BasketController.java
+++ src/main/java/com/main/braincinema/controller/BasketController.java
@@ -8,65 +8,68 @@ import java.util.Scanner;
 
 public class BasketController {
 
-    Scanner scan = new Scanner(System.in);
-    ArrayList<String> tickets = new ArrayList<>();
+//    Scanner scan = new Scanner(System.in);
+//    ArrayList<String> tickets = new ArrayList<>();
 
     public boolean addTicket() {
 
         //TODO addTicket()
 
-        System.out.println("--Add Ticket--");
-        System.out.print("film ");
-        String filmName = scan.next();
-        System.out.print("ranks ");
-        int ranks = scan.nextInt();
-        System.out.print("place ");
-        int place = scan.nextInt();
-        System.out.print("cost ");
-        BigDecimal cost = scan.nextBigDecimal();
+//        System.out.println("--Add Ticket--");
+//        System.out.print("film ");
+//        String filmName = scan.next();
+//        System.out.print("ranks ");
+//        int ranks = scan.nextInt();
+//        System.out.print("place ");
+//        int place = scan.nextInt();
+//        System.out.print("cost ");
+//        BigDecimal cost = scan.nextBigDecimal();
+//
+//        Basket basket;
+//        try {
+//            basket = new Basket(filmName, ranks, place, cost);
+//            System.out.println(basket.toString());
+//            tickets.add(basket.getFilmName());
+//            return true;
+//        } catch (Exception e) {
+//            return false;
+//        }
 
-        Basket basket;
-        try {
-            basket = new Basket(filmName, ranks, place, cost);
-            System.out.println(basket.toString());
-            tickets.add(basket.getFilmName());
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+        return true;
     }
 
     public boolean showTickets() {
-        System.out.println("--Show Ticket--");
-        for (String filmName : tickets) {
-            System.out.println(filmName);
-        }
+//        System.out.println("--Show Ticket--");
+//        for (String filmName : tickets) {
+//            System.out.println(filmName);
+//        }
         //TODO showTickets()
 
         return true;
     }
 
     public boolean deleteTicket() {
-        System.out.println("--Delete Ticket--");
-        System.out.print("enter ticket number: ");
-        int numb = scan.nextInt();
-        tickets.remove(numb - 1);
+//        System.out.println("--Delete Ticket--");
+//        System.out.print("enter ticket number: ");
+//        int numb = scan.nextInt();
+//        tickets.remove(numb - 1);
         //TODO deleteTicket()
 
         return true;
     }
 
     public boolean buyTicket() {
-        System.out.println("--Buy Ticket--");
+//        System.out.println("--Buy Ticket--");
         //TODO buyTicket()
 
-        cleanBasket();
+//        cleanBasket();
         return true;
     }
 
     public boolean cleanBasket() {
-        System.out.println("--Clean Ticket--");
-        tickets.clear();
+        //TODO cleanBasket()
+//        System.out.println("--Clean Ticket--");
+//        tickets.clear();
         return true;
     }
 }
