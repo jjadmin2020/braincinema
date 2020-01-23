a0d9a5a60dca49d4863073fcd9b051186a530912
Fedorenko Oleksandr <Oleksandr.Fedorenko@onseo.biz>
2020-01-23 18:07:34 +0200

    Test Jdbc

--- src/main/java/com/main/braincinema/Main.java
+++ src/main/java/com/main/braincinema/Main.java
@@ -1,22 +1,18 @@
 package com.main.braincinema;
 
-import com.main.braincinema.controller.SeatController;
-import com.main.braincinema.entity.Seat;
-import com.main.braincinema.entity.*;
+import com.main.braincinema.controller.UserController;
+import com.main.braincinema.repository.db.utils.H2Utils;
 
 /**
  * Created by OFedorenko on 01/16/20 braincinema.
  */
 public class Main {
     public static void main(String[] args) {
-        Auditorium auditorium = new Auditorium("2", 7, 7);
-        SeatController seatController = new SeatController();
-        auditorium.generateSeats(auditorium.getRanks(),auditorium.getPlacesInRank());
-        auditorium.setOccupiedSeat(1001);
-        auditorium.getSeats().get(7007).setType("Vip");
-        System.out.println(auditorium.getSeats().get(1001));
-        System.out.println(auditorium.getSeats().get(7007));
-        System.out.println("Amount of free seats: = " + auditorium.freeSeats());
-        System.out.println("Amount of occupied seats: = " + auditorium.occupiedSeats());
+        H2Utils.initializeDb();
+        UserController controller = new UserController();
+        controller.addUser();
+        controller.addUser();
+        controller.addUser();
+        controller.findUser();
     }
 }
--- src/main/java/com/main/braincinema/entity/User.java
+++ src/main/java/com/main/braincinema/entity/User.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 @Data
 public class User {
+    private Long id;
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
