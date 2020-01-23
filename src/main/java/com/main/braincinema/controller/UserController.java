029ffbf14fb388d0e5d1e9c5110d7e8a2abbac03
Aleksandr <aleksandrburlutskiy@gmail.com>
2020-01-23 15:50:07 +0200

    VJG- Chang method AddUser in class UserController, add field ID and constructor in class User.

--- src/main/java/com/main/braincinema/controller/UserController.java
+++ src/main/java/com/main/braincinema/controller/UserController.java
@@ -3,19 +3,43 @@ package com.main.braincinema.controller;
 import com.main.braincinema.entity.User;
 import java.util.Scanner;
 
-
 public class UserController {
     public boolean addUser() {
         User user = new User();
-        System.out.println("input user name: ");
+        System.out.println("Введите Ваше имя: ");
         Scanner scanner = new Scanner(System.in);
         user.setFirstName(scanner.nextLine());
+
+
+        System.out.println("Введите Вашу фамилию: ");
+        user.setLastName(scanner.nextLine());
+
+        System.out.println("Введите дату рождения: ");
+        user.setDateOfBirth(scanner.nextLine());
+
+        System.out.println("Введите Ваш город: ");
+        user.setCity(scanner.nextLine());
+
+        System.out.println("Введите номер телефона: ");
+        if (scanner.hasNextInt()) {
+            user.setPhoneNumber(scanner.nextLine());
+        } else {
+            System.out.println("Вы ввели не цифры ");
+        }
+
+        System.out.println("Введите  e-mail: ");
+        user.setEMail(scanner.nextLine());
+
+        System.out.println("Придумайте пароль: ");
+        user.setPassword(scanner.nextLine());
+
+        System.out.println("Сверте Ваши данные" + user.toString());
         return true;
     }
 
     public boolean deleteUser() {
         User user = new User();
-        System.out.println("input user name: ");
+        System.out.println("Введите Вашу фамилию: ");
         Scanner scanner = new Scanner(System.in);
         user.setFirstName(scanner.nextLine());
         return true;
@@ -23,14 +47,15 @@ public class UserController {
 
     public boolean findUser() {
         User user = new User();
-        System.out.println("input user name: ");
+        System.out.println("Введите Вашу фамилию: ");
         Scanner scanner = new Scanner(System.in);
         user.setFirstName(scanner.nextLine());
         return true;
     }
+
     public boolean updateUser() {
         User user = new User();
-        System.out.println("input user name: ");
+        System.out.println("Введите Вашу фамилию: ");
         Scanner scanner = new Scanner(System.in);
         user.setFirstName(scanner.nextLine());
         return true;
--- src/main/java/com/main/braincinema/entity/User.java
+++ src/main/java/com/main/braincinema/entity/User.java
@@ -2,9 +2,11 @@ package com.main.braincinema.entity;
 
 import lombok.Data;
 import java.util.Date;
+import java.util.Map;
 
 @Data
 public class User {
+    private int id;
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
@@ -12,4 +14,48 @@ public class User {
     private int phoneNumber;
     private String eMail;
     private String password;
+    private Map<Integer,User> users;
+
+    public User(String firstName,
+                String lastName,
+                Date dateOfBirth,
+                String city,
+                int phoneNumber,
+                String eMail,
+                String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.city = city;
+        this.phoneNumber = phoneNumber;
+        this.eMail = eMail;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public User(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setDateOfBirth(String nextLine) {
+    }
+
+    public void setPhoneNumber(String nextLine) {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", city='" + city + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", eMail='" + eMail + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
