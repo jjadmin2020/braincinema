891b2b38e0e71ead8758a156182be46c86ce45cd
Alex19811 <49365019+Alex19811@users.noreply.github.com>
2020-01-23 19:09:14 +0200

    VJG-11

--- src/main/java/com/main/braincinema/entity/User.java
+++ src/main/java/com/main/braincinema/entity/User.java
@@ -16,7 +16,6 @@ public class User {
     private String password;
     private Map<Integer,User> users;
 
-
     public User(String firstName,
                 String lastName,
                 Date dateOfBirth,
